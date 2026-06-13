b208af53bde10475fac150e0cb47db662921268e
Marius Jochheim <marius.jochheim@gmail.com>
2026-06-13 19:39:29 +0200

    feat: update deployment scripts and configurations for S3 and production environment

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'http://mj-udacity-udagram-api-env.eba-wcfdcn9n.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
